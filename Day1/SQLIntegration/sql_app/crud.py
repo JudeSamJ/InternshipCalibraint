bfd1f514202f8e2054eb3b0dd9b2f40190286497
JudeSamJ <judesamuelsjj@gmail.com>
2024-06-27 17:33:49 +0530

    Success

---
+++ Day1/SQLIntegration/sql_app/crud.py
@@ -0,0 +1,50 @@
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    fake_hashed_password = user.password + "notreallyhashed"
+    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def get_items(db: Session, skip: int = 50, limit: int = 100):
+    return db.query(models.Item).offset(skip).limit(limit).all()
+
+
+def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
+    db_item = models.Item(**item.model_dump(), owner_id=user_id)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+def phone_no(db: Session,phoneno: schemas.PhonenoCreate):
+    db_phoneno = models.Phoneno(phone_no = phoneno.model_dump)
+    db.add(db_phoneno)
+    db.commit()
+    db.refresh(db.phoneno)
+    return db_phoneno
+
+def get_address(db: Session,address: schemas.GetAddress):
+    db_address = models.Address(address = address.model_dump)
+    db.add(db_address)
+    db.commit()
+    db.refresh(db.address)
+    return db_address
